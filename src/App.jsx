3bba831842326507897f9cf6165d04f6e23fcd75
Gimenez Santiago <santiago.manuel.gimenez97@gmail.com>
2023-08-31 19:15:37 -0300

    solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Carrusel from "./components/Common/SectionHome/Carrusel/Carrusel";
 import FondoFijo from "./components/Common/SectionHome/FondoFijo/FondoFijo";
 import TarjetaCategoria from "./components/Common/SectionHome/TarjetaCategoria/TarjetaCategoria";
-import TarjetaOferta from "./components/Common/SectionHome/TarjetaOferta/TarjetaOferta";
 import TarjetaProducto from "./components/Common/SectionHome/TarjetaProducto/TarjetaProducto";
 import NavBar from "./components/Layout/NavBar/NavBar";
 import AppRouter from "./Routes/AppRouter";
